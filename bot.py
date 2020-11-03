baabdf192e19db58d04945cebc6aa7a68338d82d
felixyates <71043194+felixyates@users.noreply.github.com>
2020-11-03 15:18:42 +0000

    Update - FNAF phone call added
    Just to test joining, playing sound and leaving a voice call, I've added the Five Nights at Freddy's welcome phone call in. :+1:

--- bot.py
+++ bot.py
@@ -1,55 +1,53 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+import asyncio
+#import youtube_dl
+from async_timeout import timeout
 
 TOKEN = 'NzcyODU5MzQ0NTUwNDk0MjE5.X6AzWg.rLdgR--eCLiK2d9IYztlUxOsKoA'
 
-description = '''TechBot in Python'''
+description = 'TechBot in Python'
 bot = commands.Bot(command_prefix='>', description=description)
+
+vc_is_paused = False
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
+    print('Successfully logged in as',end=" ")
+    print(bot.user.name,end=" ")
     print(bot.user.id)
     print('------')
 
 @bot.command()
 async def hello(ctx):
-    """Says 'world'"""
+    "Says 'world'"
     await ctx.send("world")
 
 @bot.command()
 async def add(ctx, left : int, right : int):
-    """Adds two numbers together."""
+    "Adds two numbers together."
     await ctx.send(left + right)
 
 @bot.command()
 async def servers(ctx):
-    """Lists some cool beans servers you should join"""
+    "Lists some cool beans servers you should join"
     await ctx.send("- gaming (#1): https://discord.gg/wWdr9bf\n- gaming (#2): https://discord.gg/bZFFgXnAVZ")
 
 @bot.command()
 async def ping(ctx):
-    """Returns 'pong' if the bot is active"""
+    "Returns 'pong' if the bot is active"
     await ctx.send("pong")
 
 @bot.command()
-async def join(ctx):
-    """Join a voice channel"""
-    user = ctx.message.author
-    channel = user.voice.channel
-    await channel.connect()
-
-@bot.command()
 async def leave(ctx):
-    """Leave a voice channel"""
+    "Leave a voice channel"
     await ctx.voice_client.disconnect()
 
 @bot.command(pass_context=True)
 @has_permissions(kick_members=True)
 async def kick(ctx, member: discord.User=None):
-    """Simple kick command"""
+    "Simple kick command"
     await ctx.guild.kick(member)
     kickMessage = "Successfully kicked" ,str(member)
     await ctx.channel.send("Successfully kicked" ,str(kickMessage))
@@ -58,10 +56,66 @@ async def kick(ctx, member: discord.User=None):
 @bot.command(pass_context=True)
 @has_permissions(ban_members=True)
 async def ban(ctx, member: discord.User=None):
-    """Simple ban command"""
+    "Simple ban command"
     await ctx.guild.ban(member)
     banMessage = "Successfully banned" ,str(member)
     await ctx.channel.send(banMessage)
     print("Successfully banned" + " " + str(member))
+
+@bot.command(pass_context=True)
+async def fnaf(ctx):
+    "Plays the FNAF phone call in the voice channel"
+    # grab the user who sent the command
+    user = ctx.message.author
+    channel = user.voice.channel
+    if channel!= None:
+        # grab user's voice channel
+        channelName=channel.name
+        await ctx.channel.send('@'+str(user)+' is in channel: '+ channelName)
+        # create StreamPlayer
+        global vc
+        vc= await channel.connect()
+        vc.play(discord.FFmpegPCMAudio('fnaf.mp3'), after=lambda e: print('done', e))
+        while vc.is_playing() == True:
+            await asyncio.sleep(1)
+        # disconnect after the player has finished
+        vc.stop()
+        await leave(ctx)
+    else:
+        await ctx.channel.send("Join a channel first.")
+        await ctx.channel.send(user).format(ctx.message.author.mention())
+
+@bot.command(pass_context=True)
+async def stop(ctx):
+    "Stops the currently playing audio in voice chat"
+    if vc.is_playing() == True:
+        vc.stop()
+        await leave(ctx)
+        vc_is_paused == False
+        await ctx.channel.send("Stopped!")
+    else:
+        await ctx.channel.send("Nothing is playing!")
+
+# @bot.command(pass_context=True)
+# async def pause(ctx):
+#     "Pauses the currently playing audio in voice chat"
+#     if vc.is_playing() == True:
+#         vc.pause()
+#         await ctx.channel.send("Paused!")
+#         global vc_is_paused
+#         vc_is_paused = True
+#     else:
+#         await ctx.channel.send("Nothing is playing!")
+
+# @bot.command(pass_context=True)
+# async def resume(ctx):
+#     "Resumes the currently playing audio in voice chat"
+#     if vc_is_paused == True:
+#         vc.resume()
+#         while vc.is_playing() != True:
+#             await asyncio.sleep(1)
+#         await ctx.channel.send("Resumed!")
+#     else:
+#         await ctx.channel.send("Nothing is playing!")
 
 bot.run(TOKEN)
