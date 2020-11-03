db4504ca6fcee5aafb9795b684d1eba5be2c9a4a
felixyates <71043194+felixyates@users.noreply.github.com>
2020-11-03 22:34:44 +0000

    Dedicated player function
    Shrinks code, added way more sound effects/songs.

--- cogs/voice.py
+++ cogs/voice.py
@@ -16,52 +16,68 @@ class Voice(commands.Cog, name="voice"):
     @commands.Cog.listener()
     async def leave(self,ctx):
         await ctx.voice_client.disconnect()
+
+    @commands.Cog.listener()
+    async def player(self,ctx,path):
+        user = ctx.message.author
+        channel = user.voice.channel
+        if channel!= None:
+            # grab user's voice channel
+            channelName=channel.name
+            await ctx.channel.send('User'+ ' is in channel: '+ channelName)
+            # create StreamPlayer
+            global vc
+            vc= await channel.connect()
+            vc.play(discord.FFmpegPCMAudio(path), after=lambda e: print('done', e))
+            while vc.is_playing() == True:
+                await asyncio.sleep(1)
+            # disconnect after the player has finished
+            vc.stop()
+            await self.leave(ctx)
+        else:
+            await ctx.channel.send("Join a channel first.")
     
     @commands.command(pass_context=True)
     async def fnaf(self,ctx):
         "Plays the FNAF phone call in the voice channel"
-        # grab the user who sent the command
-        user = ctx.message.author
-        channel = user.voice.channel
-        if channel!= None:
-            # grab user's voice channel
-            channelName=channel.name
-            await ctx.channel.send('@'+str(user)+' is in channel: '+ channelName)
-            # create StreamPlayer
-            global vc
-            vc= await channel.connect()
-            vc.play(discord.FFmpegPCMAudio('fnaf.mp3'), after=lambda e: print('done', e))
-            while vc.is_playing() == True:
-                await asyncio.sleep(1)
-            # disconnect after the player has finished
-            vc.stop()
-            await self.leave(ctx)
-        else:
-            await ctx.channel.send("Join a channel first.")
-            await ctx.channel.send(user).format(ctx.message.author.mention())
+        path = 'fnaf.mp3'
+        await self.player(ctx,path)
     
     @commands.command(pass_context=True)
     async def cats(self,ctx):
         "As per Harry's request. Don't even ask."
-        # grab the user who sent the command
-        user = ctx.message.author
-        channel = user.voice.channel
-        if channel!= None:
-            # grab user's voice channel
-            channelName=channel.name
-            await ctx.channel.send('@'+str(user)+' is in channel: '+ channelName)
-            # create StreamPlayer
-            global vc
-            vc= await channel.connect()
-            vc.play(discord.FFmpegPCMAudio('cats.mp3'), after=lambda e: print('done', e))
-            while vc.is_playing() == True:
-                await asyncio.sleep(1)
-            # disconnect after the player has finished
-            vc.stop()
-            await self.leave(ctx)
-        else:
-            await ctx.channel.send("Join a channel first.")
-            await ctx.channel.send(user).format(ctx.message.author.mention())
+        path = 'cats.mp3'
+        await self.player(ctx,path)
+    
+    @commands.command(pass_context=True)
+    async def fortnite(self,ctx):
+        "Plays the old Fortnite Christmas music in the voice channel"
+        path = 'fortnite.mp3'
+        await self.player(ctx,path)
+
+    @commands.command(pass_context=True)
+    async def boom(self,ctx):
+        "Plays the vine boom sound effect in the voice channel"
+        path = 'boom.mp3'
+        await self.player(ctx,path)
+
+    @commands.command(pass_context=True)
+    async def breakfromads(self,ctx):
+        "Plays the Spotify 'Wanna break from the ads?' clip in the voice channel"
+        path = 'breakfromads.mp3'
+        await self.player(ctx,path)
+
+    @commands.command(pass_context=True)
+    async def bruh(self,ctx):
+        "Plays the bruh sound effect in the voice channel"
+        path = 'bruh.mp3'
+        await self.player(ctx,path)
+
+    @commands.command(pass_context=True)
+    async def wifi(self,ctx):
+        "Plays the 'Get WiFi anywhere you go' clip in the voice channel"
+        path = 'wifi.mp3'
+        await self.player(ctx,path)
 
     @commands.command(pass_context=True)
     async def stop(self,ctx):
