6072b911a3cf8ffd991c5b74c35e819d18c7c693
felixyates <71043194+felixyates@users.noreply.github.com>
2021-04-12 00:10:11 +0100

    Quick update
    - Removed old .txt commands (from old help system)
    
    - Removed changelog file (never updated it)
    
    - Updated help command (again). Removed database in favour of faster, better, link to webpage. Looks much less cluttered and is easier to understand.

--- cogs/other.py
+++ cogs/other.py
@@ -1,38 +1,13 @@
-import discord
-import os
-import asyncio
+import sqlite3, random, discord, os, asyncio
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 from async_timeout import timeout
-import sqlite3
+from modules.embedvars import setembedvar
 
 def converttostr(input_seq, separator):
    # Join all the strings in list
    final_str = separator.join(input_seq)
    return final_str
-
-global helpVar
-helpVar = discord.Embed(color=0x00ff00,title="Commands",description="Prefix"+ "- >")
-helpVar.set_footer(text="Coded by TechLife.")
-
-with sqlite3.connect('commands.db') as db:
-    cursor = db.cursor()
-    for i in range(1,7):
-        rows = cursor.execute("SELECT * FROM commands WHERE id = ?",(i,))
-        moduleCmds = []
-        for row in rows:
-            command = row[1]
-            purpose = row[2]
-            moduleCmds.append(f"{command}: {purpose}\n")
-        for x in range(len(moduleCmds)):
-            moduleCmds[x] = moduleCmds[x].replace('"',"").replace(",","\n")
-        print(moduleCmds)
-        displaynames = cursor.execute("SELECT displayname FROM modules WHERE id = ?",(i,))
-        for row in displaynames:
-            displayname = row[0]
-        helpVar.add_field(name=displayname,value=''.join(moduleCmds), inline=False)
-        
-db.close()
 
 class Other(commands.Cog, name="other"):
     def __init__(self, bot):
@@ -64,6 +39,17 @@ class Other(commands.Cog, name="other"):
 
     @commands.command()
     async def help(self,ctx):
+        helpVar=discord.Embed(title="Commands", url="https://www.techlifeyt.com/techbot-commands", description="The link above will take you to a table with all of TechBot's commands.\nThe default command prefix is >", color=0x00ff00)
+        helpVar.set_author(name="TechLife", url="https://www.techlifeyt.com/links", icon_url="https://www.techlifeyt.com/wp-content/uploads/2020/04/cropped-TechLife-150x150-1.png")
+        randomNumber = random.randint(1,4)
+        if randomNumber == 1:
+            helpVar.add_field(name="Join the developer's server!", value="[Click here](https://www.techlifeyt.com/discord)", inline=False)
+        elif randomNumber == 2:
+            helpVar.add_field(name="Support TechBot", value="[Patreon](https://www.techlifeyt.com/patreon)", inline=False)
+        elif randomNumber == 3:
+            helpVar.add_field(name="Are you subscribed?", value="[YouTube](https://www.techlifeyt.com/youtube)", inline=False)
+        elif randomNumber == 4:
+            helpVar.add_field(name="Do you like Minecraft?", value="[Check out the TechSMP](https://www.techlifeyt.com/techsmp)", inline=False)    
         await ctx.message.channel.send(embed=helpVar)
 
 def setup(bot):
