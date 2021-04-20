0c7b3f048f0635e590e52df157cf1e6b46b48afb
felixyates <71043194+felixyates@users.noreply.github.com>
2021-04-20 19:44:08 +0100

    More owner commands
    Can change the bot's status [to different activity types: playing, listening, streaming, watching] and improvements to the botservers command.

--- cogs/owner.py
+++ cogs/owner.py
@@ -5,13 +5,93 @@ from asyncio import sleep
 from discord.ext.commands import has_permissions
 from async_timeout import timeout
 from modules.embedvars import setembedvar
-from modules.emoji import tada_animated, dnd, online, yep, nope
+from modules.emoji import tada_animated, dnd, online, yep, nope, loading
 from modules.serverJSON import loadServerJson
 
 global maintenanceStatus, onlineVar, statusChannel
 onlineVar = setembedvar("G",f"{online} Online",f"TechBot is back online and reporting for duty!",False)
 statusChannel = 788802645070053377 # techbot, status
 maintenanceStatus = 0
+
+@loop(seconds=60)
+async def status_update(self):
+
+    delay = 60 # seconds
+
+
+    if maintenanceStatus == 0:
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for >help"))
+        await sleep(delay)
+    
+
+    if maintenanceStatus == 0:
+        guildCount = await get_status_info(self,0)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{guildCount} servers"))
+        await sleep(delay)
+    
+
+    if maintenanceStatus == 0:
+        memberCount = await get_status_info(self,1)
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{memberCount} members"))
+        await sleep(delay)
+
+async def setstatus(self, ctx, status, type):
+
+    global maintenanceStatus
+
+    if status != "":
+            
+        oldstatus = status
+        spaces = ctx.message.content.split(" ")
+        if len(spaces) > 2:
+            status = ctx.message.content.split(status)
+            newStatus = f"{oldstatus}{status[1]}"
+        else:
+            newStatus = status
+
+        try:
+
+            if type == "playing":
+                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=newStatus))
+
+            elif type == "listening":
+                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=newStatus))
+
+            elif type == "watching":
+                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=newStatus))
+        
+            elif type == "streaming":
+                await self.bot.change_presence(activity=discord.Streaming(url="https://www.twitch.tv/techlifeyt",name=newStatus))
+
+            maintenanceStatus = 1
+
+            success = setembedvar("G","Success!", description = f"Changed status to `{newStatus}`.")
+
+            status_update.cancel()
+            await ctx.send(embed = success)
+
+        except Exception as e:
+
+            failure = setembedvar("R","Uh oh!", description = f"Couldn't update status; `{e}`")
+            await ctx.send(embed = failure)
+
+    else:
+            
+        maintenanceStatus = 0
+
+        if status_update.is_running() == True:
+
+            status_update.restart(self)
+            description = f"{yep} Restarted status cycle."
+        
+        else:
+
+            status_update.start(self)
+            description = f"{yep} Started status cycle."
+        
+        await ctx.send(embed = setembedvar("G","Reset Status", description = description))
+
+        
 
 async def get_status_info(self,mode):
 
@@ -27,18 +107,6 @@ async def get_status_info(self,mode):
     elif mode == 1: # memberCount
         return memberCount
 
-@loop(minutes=1)
-async def status_update(self):
-    if maintenanceStatus == 0:
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for >help"))
-        await sleep(60)
-        guildCount = await get_status_info(self,0)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{guildCount} servers"))
-        await sleep(60)
-        memberCount = await get_status_info(self,1)
-        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{memberCount} members"))
-        await sleep(60)
-
 class Owner(commands.Cog, name="owner"):
     def __init__(self, bot):
         self.bot = bot
@@ -53,8 +121,7 @@ class Owner(commands.Cog, name="owner"):
     @commands.Cog.listener()
     async def on_ready(self):
 
-        print(f'Successfully logged in as {self.bot.user.name} / {self.bot.user.id}')
-        print('------')
+        print(f"Successfully logged in as {self.bot.user.name} / {self.bot.user.id}"+"\n------")
 
         status_update.start(self)
 
@@ -94,7 +161,9 @@ class Owner(commands.Cog, name="owner"):
     @commands.is_owner()
     async def botservers(self, ctx):
 
-        serverEmbed = setembedvar("G","Servers")
+        description = f"{loading} Hold on, this could take a while!"+"\nDiscord will be ratelimiting these messages..."
+
+        await ctx.send(embed = setembedvar("G","Getting all servers...", description = description))
         servers = loadServerJson()
 
         async for guild in self.bot.fetch_guilds():
@@ -105,10 +174,8 @@ class Owner(commands.Cog, name="owner"):
             Welcome: `{server["welcome"]["enabled"]}`, `{server["welcome"]["channel"]}`, `{server["welcome"]["message"]}`
             Slur Detector : `{server["slurdetector"]["enabled"]}`, `{server["slurdetector"]["channel"]}`
             Music: `{server["music"]["enabled"]}`, `{server["music"]["channel"]}`"""
-            serverEmbed.add_field(name=guild.name,value=serverMsg)
-            server = servers[str(guild.id)]
-
-        await ctx.send(embed = serverEmbed)
+            serverEmbed = setembedvar("G", guild.name, description=serverMsg, thumbnail = guild.icon_url)
+            await ctx.send(embed = serverEmbed)
     
     @commands.command()
     @commands.is_owner()
@@ -116,7 +183,31 @@ class Owner(commands.Cog, name="owner"):
 
         to_leave = self.bot.get_guild(id)
         await to_leave.leave()
-        
+
+    @commands.command()
+    @commands.is_owner()
+    async def playing(self, ctx, status = ""):
+
+        await setstatus(self, ctx, status, "playing")
+
+    @commands.command()
+    @commands.is_owner()
+    async def listening(self, ctx, status = ""):
+
+        await setstatus(self, ctx, status, "listening")
+
+    @commands.command()
+    @commands.is_owner()
+    async def streaming(self, ctx, status = ""):
+
+        await setstatus(self, ctx, status, "streaming")
+
+    @commands.command()
+    @commands.is_owner()
+    async def watching(self, ctx, status = ""):
+
+        await setstatus(self, ctx, status, "watching")
+
             
 
 def setup(bot):
